d9c43eb194d217667ae1d9ae49e23a1bf6521069
jmfigueroaGit <figueroa.jheremiah@gmail.com>
2025-03-25 14:35:16 +0800

    Add changes

--- classes/Leaderboard.js
+++ classes/Leaderboard.js
@@ -6,233 +6,263 @@ const moment = require('moment');
 
 // Define the Leaderboard class
 class Leaderboard {
-  static tableName = 'leaderboards';
+    static tableName = 'leaderboards';
 
-  constructor({
-    id,
-    member_employee_id,
-    alias_name,
-    fiscal_year,
-    total_points,
-    approved_points,
-    for_approval_points,
-    rejected_points,
-  }) {
-    this.id = id;
-    this.member_employee_id = member_employee_id;
-    this.alias_name = alias_name;
-    this.fiscal_year = fiscal_year;
-    this.total_points = total_points;
-    this.approved_points = approved_points;
-    this.for_approval_points = for_approval_points;
-    this.rejected_points = rejected_points;
-  }
-
-  static async findAll() {
-    const query = database.from(this.tableName).orderBy('total_points', 'desc');
-
-    return this.joinWithMembers(query);
-  }
-
-  static async findById(id) {
-    const query = database
-      .from(this.tableName)
-      .where('leaderboards.id', id)
-      .first();
-
-    return this.joinWithMembers(query);
-  }
-
-  static async findByEmployeeId(memberEmployeeId) {
-    let query = database(this.tableName)
-      .where('leaderboards.member_employee_id', memberEmployeeId)
-      .first();
-
-    return this.joinWithMembers(query);
-  }
-
-  static async findByRole(roleId, top = 10) {
-    const query = database(this.tableName)
-      .orderBy('leaderboards.total_points', 'desc')
-      .limit(top);
-
-    return this.joinWithMembers(query).where('members.role_id', roleId);
-  }
-
-  static async findByAliasName(aliasName) {
-    const query = database
-      .from(this.tableName)
-      .where('leaderboards.alias_name', aliasName)
-      .first();
-
-    return this.joinWithMembers(query);
-  }
-
-  // Method to create a new leaderboard record
-  async create() {
-    const [id] = await database(Leaderboard.tableName).insert(this);
-    this.id = id;
-    return this;
-  }
-
-  async update() {
-    return database(Leaderboard.tableName)
-      .where('member_employee_id', this.member_employee_id)
-      .update(this);
-  }
-
-  async addPoints(criteria_id, role_id) {
-    const existingLeaderboard = await Leaderboard.findByEmployeeId(
-      this.member_employee_id
-    );
-    if (!existingLeaderboard) {
-      throw new ErrorResponse('Leaderboard record not found', 404);
+    constructor({
+        id,
+        member_employee_id,
+        alias_name,
+        fiscal_year,
+        total_points,
+        approved_points,
+        for_approval_points,
+        rejected_points,
+    }) {
+        this.id = id;
+        this.member_employee_id = member_employee_id;
+        this.alias_name = alias_name;
+        this.fiscal_year = fiscal_year;
+        this.total_points = total_points;
+        this.approved_points = approved_points;
+        this.for_approval_points = for_approval_points;
+        this.rejected_points = rejected_points;
     }
 
-    const isManager = role_id === 5;
+    static async findAll() {
+        const query = database
+            .from(this.tableName)
+            .orderBy('total_points', 'desc');
 
-    const criteria = await Criteria.find(criteria_id, isManager);
-
-    // Special handling for role_id 6
-    if (role_id === 6) {
-      const forApprovalPoints =
-        (existingLeaderboard.for_approval_points || 0) + criteria.points;
-      const approvedPoints = existingLeaderboard.approved_points || 0;
-      const rejectedPoints = existingLeaderboard.rejected_points || 0;
-      const totalPoints = forApprovalPoints + approvedPoints + rejectedPoints;
-
-      return this.updatePoints({
-        for_approval_points: forApprovalPoints,
-        approved_points: approvedPoints,
-        total_points: totalPoints,
-      });
+        return this.joinWithMembers(query);
     }
 
-    // Original logic for other roles
-    const forApprovalPoints =
-      (existingLeaderboard.for_approval_points || 0) +
-      (criteria.director_approval ? criteria.points : 0);
-    const approvedPoints =
-      (existingLeaderboard.approved_points || 0) +
-      (criteria.director_approval ? 0 : criteria.points);
-    const rejectedPoints = existingLeaderboard.rejected_points || 0;
-    const totalPoints = forApprovalPoints + approvedPoints + rejectedPoints;
+    static async findById(id) {
+        const query = database
+            .from(this.tableName)
+            .where('leaderboards.id', id)
+            .first();
 
-    return this.updatePoints({
-      for_approval_points: forApprovalPoints,
-      approved_points: approvedPoints,
-      total_points: totalPoints,
-    });
-  }
-
-  async approvePoints(criteria_id, reward_entry_id, status) {
-    const existingLeaderboard = await Leaderboard.findByEmployeeId(
-      this.member_employee_id
-    );
-    if (!existingLeaderboard) {
-      throw new ErrorResponse('Leaderboard record not found', 404);
+        return this.joinWithMembers(query);
     }
 
-    const rewardEntry = await RewardPoints.findById(reward_entry_id);
-    if (rewardEntry.member_employee_id !== this.member_employee_id) {
-      throw new ErrorResponse(
-        'Reward entry not eligible for point update',
-        400
-      );
+    static async findByEmployeeId(memberEmployeeId) {
+        let query = database(this.tableName)
+            .where('leaderboards.member_employee_id', memberEmployeeId)
+            .first();
+
+        return this.joinWithMembers(query);
     }
 
-    const criteria = await Criteria.find(criteria_id);
-    const rewardPoints = criteria.points;
+    static async findByRole(roleId, top = 10) {
+        const query = database(this.tableName)
+            .orderBy('leaderboards.total_points', 'desc')
+            .limit(top);
 
-    let { for_approval_points, approved_points, rejected_points } =
-      existingLeaderboard;
-
-    if (status === 'approved') {
-      for_approval_points -= rewardPoints;
-      approved_points += rewardPoints;
-    } else if (status === 'rejected') {
-      for_approval_points -= rewardPoints;
-      rejected_points += rewardPoints;
-    } else {
-      for_approval_points -= rewardPoints;
+        return this.joinWithMembers(query).where('members.role_id', roleId);
     }
 
-    const totalPoints = for_approval_points + approved_points + rejected_points;
+    static async findByAliasName(aliasName) {
+        const query = database
+            .from(this.tableName)
+            .where('leaderboards.alias_name', aliasName)
+            .first();
 
-    return this.updatePoints({
-      for_approval_points,
-      approved_points,
-      rejected_points,
-      total_points: totalPoints,
-    });
-  }
-
-  async resubmitPoints(criteria_id, reward_entry_id) {
-    const existingLeaderboard = await Leaderboard.findByEmployeeId(
-      this.member_employee_id
-    );
-    if (!existingLeaderboard) {
-      throw new ErrorResponse('Leaderboard record not found', 404);
+        return this.joinWithMembers(query);
     }
 
-    const rewardEntry = await RewardPoints.findById(reward_entry_id);
-    if (rewardEntry.member_employee_id !== this.member_employee_id) {
-      throw new ErrorResponse(
-        'Reward entry not eligible for point update',
-        400
-      );
+    // Method to create a new leaderboard record
+    async create() {
+        const [id] = await database(Leaderboard.tableName).insert(this);
+        this.id = id;
+        return this;
     }
 
-    const criteria = await Criteria.find(criteria_id);
-    const rewardPoints = criteria.points;
+    async update() {
+        return database(Leaderboard.tableName)
+            .where('member_employee_id', this.member_employee_id)
+            .update(this);
+    }
 
-    let { for_approval_points, approved_points, rejected_points } =
-      existingLeaderboard;
+    async addPoints(criteria_id, role_id) {
+        const existingLeaderboard = await Leaderboard.findByEmployeeId(
+            this.member_employee_id
+        );
+        if (!existingLeaderboard) {
+            throw new ErrorResponse('Leaderboard record not found', 404);
+        }
 
-    for_approval_points += rewardPoints;
-    rejected_points -= rewardPoints;
-    const totalPoints = for_approval_points + approved_points + rejected_points;
+        const isManager = role_id === 5;
+        const criteria = await Criteria.find(criteria_id, isManager);
 
-    return this.updatePoints({
-      for_approval_points,
-      rejected_points,
-      total_points: totalPoints,
-    });
-  }
+        // Ensure points values are initialized with 0 instead of null/undefined
+        const forApprovalPoints = existingLeaderboard.for_approval_points || 0;
+        const approvedPoints = existingLeaderboard.approved_points || 0;
+        const rejectedPoints = existingLeaderboard.rejected_points || 0;
 
-  async updatePoints(updateData) {
-    return database(Leaderboard.tableName)
-      .where('member_employee_id', this.member_employee_id)
-      .update({
-        ...updateData,
-        updated_at: moment().format('YYYY-MM-DD HH:mm:ss'),
-      });
-  }
+        let newForApprovalPoints = forApprovalPoints;
+        let newApprovedPoints = approvedPoints;
 
-  static joinWithMembers(query) {
-    const memberFields = [
-      'members.member_employee_id',
-      'members.member_firstname',
-      'members.member_lastname',
-      'members.member_email',
-      'members.role_id',
-      'members.member_title',
-      'members.member_manager_id',
-      'members.member_director_id',
-      'members.member_status',
-      'members.created_at',
-      'members.updated_at',
-    ];
+        // Special handling for role_id 6
+        if (role_id === 6) {
+            newForApprovalPoints = forApprovalPoints + criteria.points;
+        } else {
+            // Original logic for other roles
+            if (criteria.director_approval) {
+                newForApprovalPoints = forApprovalPoints + criteria.points;
+            } else {
+                newApprovedPoints = approvedPoints + criteria.points;
+            }
+        }
 
-    return query
-      .select('leaderboards.*', ...memberFields)
-      .join(
-        'members',
-        'leaderboards.member_employee_id',
-        '=',
-        'members.member_employee_id'
-      );
-  }
+        const totalPoints =
+            newForApprovalPoints + newApprovedPoints + rejectedPoints;
+
+        return this.updatePoints({
+            for_approval_points: newForApprovalPoints,
+            approved_points: newApprovedPoints,
+            total_points: totalPoints,
+        });
+    }
+
+    async approvePoints(criteria_id, reward_entry_id, status) {
+        const existingLeaderboard = await Leaderboard.findByEmployeeId(
+            this.member_employee_id
+        );
+        if (!existingLeaderboard) {
+            throw new ErrorResponse('Leaderboard record not found', 404);
+        }
+
+        const rewardEntry = await RewardPoints.findById(reward_entry_id);
+        if (rewardEntry.member_employee_id !== this.member_employee_id) {
+            throw new ErrorResponse(
+                'Reward entry not eligible for point update',
+                400
+            );
+        }
+
+        const criteria = await Criteria.find(criteria_id);
+        const rewardPoints = criteria.points;
+
+        // Ensure all point values are initialized to 0 if null/undefined
+        let forApprovalPoints = existingLeaderboard.for_approval_points || 0;
+        let approvedPoints = existingLeaderboard.approved_points || 0;
+        let rejectedPoints = existingLeaderboard.rejected_points || 0;
+
+        // Validate there are enough points to subtract
+        if (forApprovalPoints < rewardPoints) {
+            // Log the inconsistency but use available points to avoid negative values
+            console.error(
+                `Inconsistent points data: Trying to subtract ${rewardPoints} points from ${forApprovalPoints} for_approval_points`
+            );
+            forApprovalPoints = 0;
+        } else {
+            forApprovalPoints -= rewardPoints;
+        }
+
+        if (status === 'approved') {
+            approvedPoints += rewardPoints;
+        } else if (status === 'rejected') {
+            rejectedPoints += rewardPoints;
+        }
+
+        const totalPoints = forApprovalPoints + approvedPoints + rejectedPoints;
+
+        return this.updatePoints({
+            for_approval_points: forApprovalPoints,
+            approved_points: approvedPoints,
+            rejected_points: rejectedPoints,
+            total_points: totalPoints,
+        });
+    }
+
+    async resubmitPoints(criteria_id, reward_entry_id) {
+        const existingLeaderboard = await Leaderboard.findByEmployeeId(
+            this.member_employee_id
+        );
+        if (!existingLeaderboard) {
+            throw new ErrorResponse('Leaderboard record not found', 404);
+        }
+
+        const rewardEntry = await RewardPoints.findById(reward_entry_id);
+        if (rewardEntry.member_employee_id !== this.member_employee_id) {
+            throw new ErrorResponse(
+                'Reward entry not eligible for point update',
+                400
+            );
+        }
+
+        const criteria = await Criteria.find(criteria_id);
+        const rewardPoints = criteria.points;
+
+        // Ensure all point values are initialized to 0 if null/undefined
+        let forApprovalPoints = existingLeaderboard.for_approval_points || 0;
+        let approvedPoints = existingLeaderboard.approved_points || 0;
+        let rejectedPoints = existingLeaderboard.rejected_points || 0;
+
+        // Validate there are enough rejected points to subtract
+        if (rejectedPoints < rewardPoints) {
+            // Log the inconsistency but use available points to avoid negative values
+            console.error(
+                `Inconsistent points data: Trying to subtract ${rewardPoints} points from ${rejectedPoints} rejected_points`
+            );
+            rejectedPoints = 0;
+        } else {
+            rejectedPoints -= rewardPoints;
+        }
+
+        forApprovalPoints += rewardPoints;
+        const totalPoints = forApprovalPoints + approvedPoints + rejectedPoints;
+
+        return this.updatePoints({
+            for_approval_points: forApprovalPoints,
+            rejected_points: rejectedPoints,
+            total_points: totalPoints,
+        });
+    }
+
+    async updatePoints(updateData) {
+        // Ensure no negative values are saved to the database
+        Object.keys(updateData).forEach((key) => {
+            if (typeof updateData[key] === 'number' && updateData[key] < 0) {
+                console.error(
+                    `Attempted to update ${key} with negative value: ${updateData[key]}`
+                );
+                updateData[key] = 0;
+            }
+        });
+
+        return database(Leaderboard.tableName)
+            .where('member_employee_id', this.member_employee_id)
+            .update({
+                ...updateData,
+                updated_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+            });
+    }
+
+    static joinWithMembers(query) {
+        const memberFields = [
+            'members.member_employee_id',
+            'members.member_firstname',
+            'members.member_lastname',
+            'members.member_email',
+            'members.role_id',
+            'members.member_title',
+            'members.member_manager_id',
+            'members.member_director_id',
+            'members.member_status',
+            'members.created_at',
+            'members.updated_at',
+        ];
+
+        return query
+            .select('leaderboards.*', ...memberFields)
+            .join(
+                'members',
+                'leaderboards.member_employee_id',
+                '=',
+                'members.member_employee_id'
+            );
+    }
 }
 
 module.exports = Leaderboard;
--- utils/send_email.js
+++ utils/send_email.js
@@ -8,49 +8,61 @@ const username = process.env.NODEMAILER_USERNAME;
 const host = process.env.NODEMAILER_HOST;
 const port = process.env.NODEMAILER_PORT;
 
+// Credentials if mailtrap is used
+const mailtrapHost = process.env.MAILTRAP_HOST;
+const mailtrapPort = process.env.MAILTRAP_PORT;
+const mailtrapUser = process.env.MAILTRAP_USER;
+const mailtrapPassword = process.env.MAILTRAP_PASSWORD;
+
 const sendMail = async ({
-  to,
-  body,
-  html,
-  subject,
-  attachments = undefined,
-  cc = undefined,
+    to,
+    body,
+    html,
+    subject,
+    attachments = undefined,
+    cc = undefined,
 }) => {
-  if (typeof to == 'undefined')
-    throw new ErrorResponse('Email is required', 400);
+    if (typeof to == 'undefined')
+        throw new ErrorResponse('Email is required', 400);
 
-  if (typeof html == 'undefined')
-    throw new ErrorResponse('Email body is required', 400);
+    if (typeof html == 'undefined')
+        throw new ErrorResponse('Email body is required', 400);
 
-  if (typeof subject == 'undefined')
-    throw new ErrorResponse('Email subject is required', 400);
+    if (typeof subject == 'undefined')
+        throw new ErrorResponse('Email subject is required', 400);
 
-  const transporter = nodemailer.createTransport({
-    host: host,
-    port: port,
-    secure: false,
-    auth: {
-      user: username,
-      pass: password,
-    },
-  });
+    // const transporter = nodemailer.createTransport({
+    //     host: host,
+    //     port: port,
+    //     secure: false,
+    //     auth: {
+    //         user: username,
+    //         pass: password,
+    //     },
+    // });
 
-  const mailOptions = {
-    from: `"${username}" <${email}>`,
-    to:
-      process.env.NODE_ENV === 'development'
-        ? process.env.NODE_TESTER_EMAILS
-        : to,
-    subject: subject,
-    text: body,
-    html: html,
-    attachments: attachments,
-  };
+    const mailOptions = {
+        from: `"${username}" <${email}>`,
+        to: to,
+        subject: subject,
+        text: body,
+        html: html,
+        attachments: attachments,
+    };
 
-  mailOptions.cc =
-    process.env.NODE_ENV === 'development' ? process.env.TESTER_EMAILS_CC : cc;
+    mailOptions.cc = cc;
 
-  return await transporter.sendMail(mailOptions);
+    const transporter = nodemailer.createTransport({
+        host: mailtrapHost,
+        port: mailtrapPort,
+        secure: false,
+        auth: {
+            user: mailtrapUser,
+            pass: mailtrapPassword,
+        },
+    });
+
+    return await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendMail;
